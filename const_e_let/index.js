308ab8133f9590412b01f2873227344c268e117c
renanbarrostech-cmd <renanbarros.tech@gmail.com>
2026-09-12 11:38:51 -0300

    const e let

---
+++ const_e_let/index.js
@@ -0,0 +1,12 @@
+// let pode trocar o valor da variável
+
+// const mantém um valor único na variável
+
+let idioma = 'Hello word'
+
+if (1 + 1 === 2) {
+    idioma = 'Olá mundo'
+}
+
+console.log(idioma)
+ 
