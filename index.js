e4dffc115f57d215483bec543b83249c1a9f08ec
renanbarrostech-cmd <renanbarros.tech@gmail.com>
2026-09-12 11:09:35 -0300

    Constantes e Variaveis de Escopo (const e let)

---
+++ index.js
@@ -0,0 +1,15 @@
+if (2 + 2 === 4) {
+    var message = "hello word"
+    console.log(message)
+}
+
+console.log(message)
+
+// if (2 + 2 === 4) {
+//     const message2 = "Olá mundo"
+//     console.log(message2)
+// }
+
+// console.log(message2)
+
+// somente var consegue executar o script fora do escopo.
