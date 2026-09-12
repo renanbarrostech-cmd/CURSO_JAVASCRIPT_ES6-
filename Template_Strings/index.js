cbf2212f2460c9e6eb82d6101c1541d315fb4b25
renanbarrostech-cmd <renanbarros.tech@gmail.com>
2026-09-12 12:02:17 -0300

    Template Strings

---
+++ Template_Strings/index.js
@@ -0,0 +1,16 @@
+const name = "John";
+const age = 18;
+const city = "São Paulo";
+
+const message =
+  "Eu sou " + name + ", Tenho " + age + " anos, " + "e sou de " + city;
+
+message
+
+// com template string
+
+const message2 = `Eu sou ${name}, Tenho ${age} anos, e sou de ${city}`
+
+message2
+
+console.log(`2 + 2 é igual a ${2 + 2}`)
