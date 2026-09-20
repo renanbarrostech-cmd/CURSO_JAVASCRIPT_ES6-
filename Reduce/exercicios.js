99f12dc9f99baec6ae473af1ce54627b8a1137b0
renanbarrostech-cmd <renanbarros.tech@gmail.com>
2026-09-20 07:05:36 -0300

    Exercicios Reduce

---
+++ Reduce/exercicios.js
@@ -0,0 +1,39 @@
+// 1. Garde em uma variavel a soma de todos os valores
+// contidos na lista "numeros", e logue-a no console.
+
+const numeros = [3, 4, 6, 8];
+
+// 2. Guarde em uma variavel uma string que contenha todos
+// os valores da lista "letras", e logue-as no console.
+
+const letras = ['a', 'b', 'c', 'd'];
+// Exemplo: "abcd"
+
+// 3. Guarde em uma variavel a soma das idades de todas as 
+// pessoas que votaram (votou = true), e logue-a no console.
+
+const pessoas = [
+    { nome: "Bob", idade: 30, votou: true },
+    { nome: "Jake", idade: 32, votou: true },
+    { nome: "Kate", idade: 25, votou: false },
+    { nome: "Sam", idade: 20, votou: false },
+    { nome: "Phil", idade: 21, votou: true },
+    { nome: "Ed", idade: 55, votou: true },
+    { nome: "Tami", idade: 54, votou: true },
+    { nome: "Mary", idade: 31, votou: false },
+    { nome: "Becky", idade: 43, votou: false },
+    { nome: "Joey", idade: 41, votou: true },
+    { nome: "Jeff", idade: 30, votou: true },
+    { nome: "Zack", idade: 19, votou: false },
+];
+
+// 4. Guarde em uma variavel a soma dos preços de todos os 
+// produtos na nossa lista de desejos, e logue-as no console.
+
+const listaDeDesejos = [
+    { titlo: "Tesla Model S", preco: 90000 },
+    { titlo: "MacBook Pro", preco: 45000 },
+    { titlo: "Outro Tesla Model S", preco: 5 },
+    { titlo: "Carro de Fórmula 1", preco: 2000 },
+    { titlo: "Outro Carro de Fórmula 1", preco: 90000 },
+]
