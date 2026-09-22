0b68585e6bb191e7c1692bd1d79eb68042d3ea35
renanbarrostech-cmd <renanbarros.tech@gmail.com>
2026-09-22 20:55:31 -0300

    StartsWith e EndsWith

---
+++ StartsWith_e_EndsWith/index.js
@@ -0,0 +1,11 @@
+// startsWith
+
+const texto = "Ser ou não ser, eis a questão";
+
+const comecaComSer = texto.startsWith("não ser", 7);
+
+console.log(comecaComSer);
+
+// endsWith
+const terminaComQuestao = texto.endsWith("questão");
+console.log(terminaComQuestao);
