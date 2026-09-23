29713c75e816693d13bf2c724d9a99f866611103
renanbarrostech-cmd <renanbarros.tech@gmail.com>
2026-09-22 21:14:14 -0300

    fix: Exercicios StartsWith and EndsWith

--- StartsWith_e_EndsWith/exercicios.js
+++ StartsWith_e_EndsWith/exercicios.js
@@ -3,8 +3,8 @@
 
 const mensagem = "Olá, meu nome é John!";
 
-const comecaComMeu = mensagem.startsWith('Meu')
-console.log(comecaComMeu)
+const comecaComMeu = mensagem.startsWith("Meu");
+console.log(comecaComMeu);
 
 // 2. Guarde em uma variavel se a string abaixo, a
 // partir da posição 4, começa com "meu", e logue-a no
@@ -12,13 +12,13 @@ console.log(comecaComMeu)
 
 const mensagem2 = "Olá, meu nome é Jane!";
 
-const mensagemComecaComPos4 = mensagem2.startsWith('meu', 4);
-console.log(mensagemComecaComPos4)
+const mensagemComecaComPos4 = mensagem2.startsWith("meu", 4);
+console.log(mensagemComecaComPos4);
 
 // 3. Guarde em uma variavel se a string abaixo termina
 // com "Doe", e logue-a no console.
 
 const mensagem3 = "Olá, meu nome é Doe!";
 
-const terminaComDoe = mensagem3.endsWith('Doe!')
-console.log(terminaComDoe)
+const terminaComDoe = mensagem3.endsWith("Doe!");
+console.log(terminaComDoe);
